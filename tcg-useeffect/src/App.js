3137b4052a658f65215b4e4eb284880030f81ca7
Laahkay <tshilambwanakgothatso@gmail.com>
2023-11-09 11:37:02 +0200

    count

--- tcg-useeffect/src/App.js
+++ tcg-useeffect/src/App.js
@@ -3,14 +3,18 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [calculation, setCalculation] = useState(0);
 
   useEffect(() => {
-    setTimeout(() => {
-      setCount((count) => count + 1);
-    }, 1000);
-  });
+    setCalculation(() => count * 2);
+  }, [count]); 
 
-  return <h1>I have rendered {count} times!</h1>;
+  return (
+    <>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount((c) => c + 1)}>+</button>
+      <p>Calculation: {calculation}</p>
+    </>
+  );
 }
-
 export default App;
